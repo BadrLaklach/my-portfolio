4eede9f4bd5b4b85cbc7698c9b43823b0e522718
BadrLaklach <b.laklach@edu.umi.ac.ma>
2026-02-18 21:08:42 +0000

    Update Logo component: Change logo image source path to '/my-portfolio/logo.png' for better organization.

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo({ className = "" }: { className?: string }) {
   return (
     <Image
-      src="/logo.png"
+      src="/my-portfolio/logo.png"
       alt="BL Logo"
       width={32}
       height={32}
